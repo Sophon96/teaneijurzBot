49e0f7cc6e23a3c219beba52c2400a4d5cd9bf9b
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2021-08-09 13:49:27 -0700

    boomp, assert client is not null, clean up imports

--- src/main/java/com/github/teaneijurzBot/teaneijurzBot/teaneijurzBot.java
+++ src/main/java/com/github/teaneijurzBot/teaneijurzBot/teaneijurzBot.java
@@ -5,8 +5,6 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 
-import java.util.Locale;
-
 public class teaneijurzBot {
 
     public static void main(String[] args) {
@@ -14,12 +12,30 @@ public class teaneijurzBot {
                 .login()
                 .block();
 
+        assert client != null;
+
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .map(MessageCreateEvent::getMessage)
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
                 .filter(message -> message.getContent().toLowerCase().contains("js"))
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage("fuck js"))
+                .subscribe();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> user.getId().asLong() == 302050872383242240L).orElse(false))
+                .filter(message -> message.getEmbeds().get(0).getDescription().orElse("").contains("Bump done"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("boomp success :sunglasses:"))
+                .subscribe();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> user.getId().asLong() == 302050872383242240L).orElse(false))
+                .filter(message -> message.getEmbeds().get(0).getDescription().orElse("").contains("Please wait"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("boomp fail"))
                 .subscribe();
 
         client.onDisconnect().block();
