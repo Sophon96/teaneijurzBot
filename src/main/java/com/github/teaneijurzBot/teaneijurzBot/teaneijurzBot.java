df5b48c5e6a6d7acc77df6bd9409b3d3f36731a0
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2021-08-09 15:01:40 -0700

    yuh

--- src/main/java/com/github/teaneijurzBot/teaneijurzBot/teaneijurzBot.java
+++ src/main/java/com/github/teaneijurzBot/teaneijurzBot/teaneijurzBot.java
@@ -4,6 +4,7 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.reaction.ReactionEmoji;
 
 public class teaneijurzBot {
 
@@ -36,6 +37,13 @@ public class teaneijurzBot {
                 .filter(message -> message.getEmbeds().get(0).getDescription().orElse("").contains("Please wait"))
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage("boomp fail"))
+                .subscribe();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().toLowerCase().contains("yuh"))
+                .flatMap(message -> message.addReaction(ReactionEmoji.codepoints("U+1F60E")))
                 .subscribe();
 
         client.onDisconnect().block();
